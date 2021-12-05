73ee77a1a0ff662e2046f4da45396ec2732bc1a6
Kate Sky <kateskytech@gmail.com>
2021-12-05 15:38:03 -0600

    initial commit

---
+++ apps/shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sarafan-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shop';
+}
